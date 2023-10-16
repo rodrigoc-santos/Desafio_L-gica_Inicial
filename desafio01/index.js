54f8ccacb1092400b5ea3581c247e022d92c549b
rodrigoc-santos <rodrigocoelho.20rsc@gmail.com>
2023-10-16 10:57:32 -0300

    Adicionando estrutura de input de dados para solicitar nome e nível de experiência do herói

--- desafio01/index.js
+++ desafio01/index.js
@@ -1,2 +1,50 @@
-console.log("Olá mundo!")
-console.log("Olá mundo!")
+// # 1️⃣ Desafio Classificador de nível de Herói
+
+// ## Entrada do nome e do nível de experiência do Herói
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let nomeDoHeroi;
+let xpDoHeroi;
+
+rl.question('Digite o nome do Herói : ', (nome) => {
+    nomeDoHeroi = nome;
+    rl.question('Digite o XP do Herói : ', (nivelXp) => {
+      xpDoHeroi = nivelXp;      
+      if (xpDoHeroi < 5000) {
+          console.log("O Herói de nome " + nomeDoHeroi + " está no nível de ")
+          console.log("Iniciante")
+      }
+      rl.close();
+    });
+});
+
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+
+// ## Objetivo
+
+// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 6.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000= Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói de nome **{nome}** está no nível de **{nivel}**"
